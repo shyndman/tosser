015fd8a7bd05e8b038207dc1e8d31681205b77d0
Scott Hyndman <shyndman@google.com>
2015-02-14 14:03:50 -0800

    Documentation

--- tosser.js
+++ tosser.js
@@ -19,10 +19,21 @@
       replacementPattern = buildReplacementPattern();
 
   /**
-   * options:
-   *     container: Element
-   *     containerUnits: Object<string, number>
-   *     mutate: boolean (default false)
+   * Evaluates an object of style properties (name to value), like what you would
+   * supply to `jQuery.css()`. If calc values are found, they will be converted
+   * to px.
+   *
+   * Using options.containerUnits will result in drastically improved
+   * performance when evaluating multiple calcs against a single container. It
+   * can be generated using `resolveContainerUnits`.
+   *
+   * @param {Object<string, string>} styles
+   * @param {Object} options
+   *     @param {Element} options.container
+   *     @param {Object<string, number>=} options.containerUnits
+   *     @param {boolean} options.mutate If the style object should be mutated
+   *         (default false)
+   * @return {Object<string, string>}
    */
   tosser.evaluateStyles = function(styles, options) {
     var units = options.containerUnits ||
@@ -40,11 +51,21 @@
   };
 
   /**
-   * Evaluates a calc expression in the context of a container.
-   * options:
-   *     container: Element
-   *     containerUnits: Object<string, number>
-   *     vertical: boolean (default: false)
+   * Evaluates a calc expression in the context of a container. Either
+   * options.container or options.containerUnits must be supplied.
+   *
+   * Using options.containerUnits will result in drastically improved
+   * performance when evaluating multiple calcs against a single container. It
+   * can be generated using `resolveContainerUnits`.
+   *
+   * @param {string} expression A calc expression.
+   * @param {Object} options
+   *     @param {Element} options.container
+   *     @param {Object<string, number>=} options.containerUnits
+   *     @param {boolean} options.vertical If this is a vertical styling
+   *         property, where % units are based on the container's height
+   *         (default: false)
+   * @return {string} The result of the expression, in px units.
    */
   tosser.evaluateCalc = function(expression, options) {
     var units = options.containerUnits ||
@@ -64,18 +85,16 @@
             default:
               return value * (units[unit] || knownUnits[unit]);
           }
-        }));
+        })) + 'px';
   };
 
   /**
-   * Resolves and returns units that are relative to the container, or can only
-   * be determined at a point in time.
-   *
-   * Useful when evaluating multiple calcs against a single container, and
-   * can be supplied to `evaluateCalc` as options.containerUnits to avoid
-   * unnecessary calculation.
+   * Resolves and returns units that are relative to the container, or that can
+   * only be determined at a point in time.
    */
   tosser.resolveContainerUnits = function(container) {
+    // Use a <tosser-div> so we don't unintentionally have selectors match,
+    // potentially skewing the results.
     var el = document.createElement('tosser-div');
     el.style.position = 'absolute';
     el.style.display = 'block';
@@ -99,7 +118,11 @@
   }
 
   /**
+   * Builds the measurement divs used to determine unit lengths in pixels.
    *
+   * NOTE: Ever div is 100 units wide, which is then divided by 100 in order to
+   * approximate the subpixel width of some units, without having to resort to
+   * Element.getBoundingClientRect() (which is slow).
    */
   function buildMeasurementHtml() {
     var measurers = unknownUnits.map(function(unit) {
@@ -107,10 +130,10 @@
     });
     measurers.push('<div class="tosser--percent" style="width:100%;height:100%"></div>');
     return measurers.join('');
-  }
+  };
 
   /**
-   *
+   * Builds the pattern used to detect CSS lengths in a calc expression.
    */
   function buildReplacementPattern() {
     var units = unknownUnits.concat(Object.keys(knownUnits)).join('|');
